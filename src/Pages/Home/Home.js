3f87de87f06cee6490e9245387c82f4b4083f485
riyad1721 <reyadhasan7254@gmail.com>
2022-04-04 22:42:12 +0600

    Doctors portal project starting

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Services from '../Services/Services';
+import Navigation from '../Shared/Navigation/Navigation';
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <Services></Services>
+
+        </div>
+    );
+};
+
+export default Home;
