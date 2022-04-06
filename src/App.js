a81febaa0715cd2e9c8be142ea30619044a8a099
riyad1721 <reyadhasan7254@gmail.com>
2022-04-07 02:32:21 +0600

    firebase added for logIn

--- src/App.js
+++ src/App.js
@@ -2,20 +2,27 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './Pages/Home/Home/Home';
 import Appointment from './Pages/Appointment/Appointment/Appointment';
+import Login from './Pages/Login/Login/Login'
+import Register from './Pages/Login/Register/Register';
+import AuthProvider from './contexts/AuthProvider/AuthProvider';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/appointment" element={<Appointment />} />
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/appointment" element={<Appointment />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
 
-        </Routes>
+          </Routes>
 
-      </Router>
+        </Router>
+      </AuthProvider>
 
     </div>
   );
